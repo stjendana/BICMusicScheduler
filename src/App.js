ee2d24fe0cfca6525a18dac5f8f1630a01ea3869
kevinismantara <ismantara.kevin@yahoo.com>
2019-08-03 23:34:19 -0700

    Add router functionality

--- src/App.js
+++ src/App.js
@@ -1,16 +1,26 @@
 import React, { Component } from 'react';
 import Welcome from './Components/Welcome'
 import Home from './Components/Home'
-import logo from './logo.svg';
+import createBrowserHistory from 'history/createBrowserHistory'
+import { BrowserRouter as Router } from 'react-router-dom'
+import { Route } from 'react-router'
+
 import './App.css';
+
+export const history = createBrowserHistory()
 
 class App extends Component {
   render(){
     return (
-      <Home/>
+        <Router history={history}>
+            <div>
+                <Route exact path='/' component={Home} />
+                <Route exact path='/welcome' component={Welcome} />
+            </div>    
+        </Router>   
     )
   }
   
 }
 
-export default App;
+export default App
